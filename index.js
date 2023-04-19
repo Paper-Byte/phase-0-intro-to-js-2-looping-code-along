b545c88aa39c3d5ec9e9e2d6a7c2c3deb2d09710
Noah <noahhughesph@gmail.com>
2023-04-18 23:07:17 -0400

    all test passing

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!` );
+    debugger;
+}
+
+const gifts = ['teddy bear', 'drone', 'doll']
+
+const wrapGifts = (gifts) => {
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const writeCards = (names, event) => {
+    const letterNames = [];
+    for(let i = 0; i < names.length; i++){
+        letterNames[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return letterNames;
+}
+
+const countDown = (int) => {
+    let count = int;
+    while(count >= 0){
+        console.log(count)
+        count--;
+    }
+}
